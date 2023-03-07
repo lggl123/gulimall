ee61fa66b2726f3b5cb470396f02add62152db09
lggl123 <127193098+lggl123@users.noreply.github.com>
2023-03-07 18:46:33 +0800

    Update demo.java

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/vo/demo.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/vo/demo.java
@@ -3,5 +3,6 @@ package com.atguigu.gulimall.product.vo;
 public class demo {
     public static void main(String[] args) {
         System.out.println("1111");
+        System.out.println("1111");
     }
 }
